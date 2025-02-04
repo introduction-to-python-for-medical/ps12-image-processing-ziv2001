bd5b63bfb70e24552b9de7ad48fd6761fd8d17b3
ziv2001 <ziv222444666@gmail.com>
2025-02-04 19:35:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from image_utils import load_image, edge_detection, preprocess_image
 
+image = load_image('candy1.png')
+image = preprocess_image(image)
+edges = edge_detection(image)
+print(edges)
